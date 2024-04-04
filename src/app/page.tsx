d694352adada1cc75226f6786dee4baf269c9361
anshnk <ansh@anshnk.dev>
2024-04-03 21:46:38 -0500

    Figured out how to add react-winbox, (idk how to make windows load)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import "@/styles/globals.css"
 import { Globe } from "lucide-react"
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+const WinBox = dynamic(() => import('react-winbox'), {ssr: false});
+import 'winbox/dist/css/winbox.min.css';
+
 import {
   Menubar,
   MenubarCheckboxItem,
@@ -21,12 +27,18 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 
 export default function Home() {
+  
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <body>
 <div>
@@ -119,23 +131,37 @@ export default function Home() {
     </Menubar>
     </div>
     <div className="flex h-screen flex-col items-center justify-center">
-			<div className="flex-grow" />
-    <Card className="mx-auto flex h-96 w-96 flex-col items-center justify-center">
-  <CardHeader>
-    <CardTitle className="mx-auto mb-2 flex flex-col items-center justify-center">
-    <Globe className="h-14 w-14" />
-      <h2 className="mt-2 text-3xl font-semibold">What the Sigma!?</h2>
-    </CardTitle>
-    
-    <CardDescription className="text-center">validOS NextJS Rewrite</CardDescription>
-  </CardHeader>
-  <CardContent className="mt-8 text-center text-base">
-    <p>Just a nice rewrite of the entire source code.<br></br>validOS NexJS Rewrite uses tailwind, typescript, and shadCN.</p>
-  </CardContent>
-</Card>
-    <div className="flex-grow" />
-		</div>
+        <div className="flex-grow" />
+        {isClient && (
+          <>
+            <Card className="mx-auto flex h-96 w-96 flex-col items-center justify-center">
+              <CardHeader>
+                <CardTitle className="mx-auto mb-2 flex flex-col items-center justify-center text-center">
+                  <Globe className="h-14 w-14" />
+                  <h2 className="mt-2 text-3xl font-semibold">What the Sigma!?</h2>
+                </CardTitle>
+                <CardDescription className="text-center">validOS NextJS Rewrite</CardDescription>
+              </CardHeader>
+              <CardContent className="mt-8 text-center text-base">
+                <p>Just a nice rewrite of the entire source code.<br></br>validOS NexJS Rewrite uses tailwind, typescript, and shadCN.</p>
+              </CardContent>
 
+              <CardContent>
+                <Button className="w-full" onClick={() => <WinBox></WinBox>}>
+                  
+                </Button>
+              </CardContent>
+            </Card>
+
+
+
+            <script type="module">
+              {`import WinBox from "https://unpkg.com/winbox@0.2.82/src/js/winbox.js";`}
+            </script>
+          </>
+        )}
+        <div className="flex-grow" />
+      </div>
     </body>
   )
 }
